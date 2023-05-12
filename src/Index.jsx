11fb942393520393c1b32db12607b9b7f09c27ed
avillanueva-2021436 <avillanueva-2021436@kinal.edu.gt>
2023-05-12 14:29:40 -0600

    Se agregaron las rutas

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState, createContext, useEffect } from 'react'
+import App from './App'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { NotFound } from './pages/NotFound';
+import { HomePage } from './pages/HomePage';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
+
+export const AuthContext = createContext();
 
 export const Index = () => {
+    const [loggedIn, setLoggedIn] = useState(false)
+    const [dataUser, setDataUser] = useState({
+      name: '',
+      username: '',
+      role: ''
+    })
+    useEffect(()=> {
+      let token = localStorage.getItem('token')
+      if(token) setLoggedIn(true)
+    }, [])
+
+    const routes = createBrowserRouter([
+        {
+          path: '/',
+          element: <App/>,
+          errorElement: <NotFound/>,
+          children: [
+            {
+              path: '/',
+              element: <HomePage/>
+            },
+            {
+              path: '/register',
+              element: <RegisterPage></RegisterPage>
+            },
+            {
+              path: '/login',
+              element: <LoginPage></LoginPage>
+            },
+          ]
+        }
+      ])
   return (
-    <h1>Index</h1>
+    <AuthContext.Provider value={{loggedIn, setLoggedIn, dataUser, setDataUser}}>
+      <RouterProvider router={routes}/>
+    </AuthContext.Provider>
   )
 }
+
