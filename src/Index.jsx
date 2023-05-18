c0e1e811edb5946cc83767c38cb2115c2689f146
Angel <avillanueva-2021436@kinal.edu.gt>
2023-05-18 12:52:10 -0600

    Se agrego las rutas de event

--- src/Index.jsx
+++ src/Index.jsx
@@ -5,45 +5,70 @@ import { NotFound } from './pages/NotFound';
 import { HomePage } from './pages/HomePage';
 import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
+import { DashboardPage } from './pages/DashboardPage';
+import { ListEvent } from './pages/Event/ListEvent';
+import { UpdateEvent } from './pages/Event/updateEvent';
+import { EventPage } from './pages/Event/EventPage';
 
 export const AuthContext = createContext();
 
 export const Index = () => {
-    const [loggedIn, setLoggedIn] = useState(false)
-    const [dataUser, setDataUser] = useState({
-      name: '',
-      username: '',
-      role: ''
-    })
-    useEffect(()=> {
-      let token = localStorage.getItem('token')
-      if(token) setLoggedIn(true)
-    }, [])
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [dataUser, setDataUser] = useState({
+    name: '',
+    username: '',
+    role: ''
+  })
+  useEffect(() => {
+    let token = localStorage.getItem('token')
+    if (token) setLoggedIn(true)
+  }, [])
 
-    const routes = createBrowserRouter([
+  const routes = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <NotFound />,
+      children: [
         {
           path: '/',
-          element: <App/>,
-          errorElement: <NotFound/>,
+          element: <HomePage />
+        },
+        {
+          path: '/register',
+          element: <RegisterPage></RegisterPage>
+        },
+        {
+          path: '/login',
+          element: <LoginPage></LoginPage>
+        },
+        {
+          path: '/dashboard',
+          element: loggedIn ? <DashboardPage /> : <LoginPage />,
           children: [
             {
-              path: '/',
-              element: <HomePage/>
-            },
-            {
-              path: '/register',
-              element: <RegisterPage></RegisterPage>
-            },
-            {
-              path: '/login',
-              element: <LoginPage></LoginPage>
+              path: 'event',
+              element: <EventPage></EventPage>,
+              children: [
+                {
+                  path: '',
+                  exact: true,
+                  element: <ListEvent />
+                },
+                {
+                  path: 'updateEvent/:id',
+                  element: <UpdateEvent />
+                }
+              ]
             },
           ]
         }
-      ])
+      ]
+    }
+  ])
   return (
-    <AuthContext.Provider value={{loggedIn, setLoggedIn, dataUser, setDataUser}}>
-      <RouterProvider router={routes}/>
+    <AuthContext.Provider value={{ loggedIn, setLoggedIn, dataUser, setDataUser }}>
+      <RouterProvider router={routes} />
     </AuthContext.Provider>
   )
 }
